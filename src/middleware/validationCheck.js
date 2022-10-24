28b9fadb20bb2d38fbd2f57a47c5ce99a5858087
Dmitriy Chumak <chumak.exceedteam@gmail.com>
2022-10-24 12:37:43 +0300

    refactoring: refactoring validation module

--- src/middleware/validationCheck.js
+++ src/middleware/validationCheck.js
@@ -1,12 +1,11 @@
 const { validationResult } = require('express-validator');
 
 const validateResult = (req, res, next) => {
-  try {
-    validationResult(req).throw();
-    return next();
-  } catch (err) {
-    res.status(400).send({ errors: err.array() });
-  }
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    next();
+  } 
+  res.status(400).send({ message: 'Bad validation' });
 }
 
 module.exports = validateResult;
