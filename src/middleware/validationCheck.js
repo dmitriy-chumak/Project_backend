61fd295bb93f91ca65279ac742eaffb8f1adca7a
Dmitriy Chumak <chumak.exceedteam@gmail.com>
2022-10-26 10:18:10 +0300

    fix: fix code in validation

--- src/middleware/validationCheck.js
+++ src/middleware/validationCheck.js
@@ -2,11 +2,10 @@ const { validationResult } = require('express-validator');
 
 const validateResult = (req, res, next) => {
   const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    next();
-  } else {
-    res.status(400).send({ message: 'Bad validation' });
-  }
+  if (!errors.isEmpty()) {
+    return res.status(400).send({ message: 'Bad validation' });
+  } 
+  next();
 }
 
 module.exports = validateResult;
